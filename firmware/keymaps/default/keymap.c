f9d3c09f02a0d67495b4c4347abb141e58d4dd32
siddhantverma04 <siddhantverma04@gmail.com>
2026-09-09 17:35:05 -0700

    Added OLED

--- firmware/keymaps/default/keymap.c
+++ firmware/keymaps/default/keymap.c
@@ -7,3 +7,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_7, KC_8, KC_9
     )
 };
+
+#ifdef OLED_ENABLE
+bool oled_task_user(void) {
+    oled_write_ln("MY HACKPAD", false);
+    oled_write_ln("QMK READY!", false);
+
+    return false;
+}
+#endif
