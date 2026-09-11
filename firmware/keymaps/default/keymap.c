626c9f36d4acaf19339c101f0e5ddf63a0a24f4e
siddhantverma04 <siddhantverma04@gmail.com>
2026-09-10 17:52:34 -0700

    Added controls to the keys
    Might change later

--- firmware/keymaps/default/keymap.c
+++ firmware/keymaps/default/keymap.c
@@ -1,17 +1,31 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    
     [0] = LAYOUT(
-        UG_TOGG, UG_NEXT, UG_PREV,
-        UG_HUEU, UG_HUED, UG_SATU,
-        UG_SATD, UG_VALU, UG_VALD
+        KC_1, KC_2, KC_3,
+        KC_4, KC_5, KC_6,
+        KC_7, KC_8, KC_9
+    ),
+
+    [1] = LAYOUT(
+        UG_TOGG, UG_NEXT, UG_HUEU,
+        UG_HUED, UG_SATU, UG_SATD,
+        UG_VALU, UG_VALD, KC_TRNS
     )
 };
 
 #ifdef OLED_ENABLE
 bool oled_task_user(void) {
     oled_write_ln("MY HACKPAD", false);
-    oled_write_ln("RGB CONTROL", false);
+
+    if (get_higtest_layer(layer_state) == 1){
+        oled_write_ln("RGB CONTROL", false);
+    } else {
+        oled_write_ln("READY", false);
+    }
+
     return false;
+
 }
 #endif
